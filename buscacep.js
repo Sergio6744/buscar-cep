b8e9cba3a9010c08dbeb974ddc71322069c02133
Sérgio <101438156+Sergio6744@users.noreply.github.com>
2023-02-19 21:12:48 -0300

    apenas algumas melhorias de codigo no CSS e no Javscript
    ajustando o CSS da pagina e o codigo javascript para deixalo mais organizado.

--- buscacep.js
+++ buscacep.js
@@ -1,55 +1,37 @@
-function pegar() {
-
-   let cep = document.querySelector('#cep')
-   
-   let resp = document.querySelector('#resp')
-   
-   cep = cep.value
-   let len = `${cep}`
-   
-   if (len.length != 8) {
-   
-     resp.style.color = "red"
-     resp.innerHTML = ""
-     resp.innerHTML += "o cep deve ter 8 caracteres"
-     len = 0
-     
-   } else {
-              fetch(`https://viacep.com.br/ws/${cep}/json/`)
-      .then(response => response.text())
-      .then(response => {
-   
-         resp.style.color="black"
-         resp.innerHTML = ""
-         let obj =JSON.parse(response)
-         len = 0
-         
-         let c = 0
-         
-         for (i in obj) {
-
-            c = c + 1
-            resp.innerHTML += `${i} : ${obj[i]}` 
-            if (obj[i] == "") {
-               resp.innerHTML += "?????<br>"
-            } if (obj[i] != "") {
-               resp.innerHTML += "<br>"
+document.addEventListener("keydown", (t)=>{
+   if(t.key === 'Enter') {
+      let cep = document.querySelector("#cep").value
+      let rspst = document.querySelector("#resp")
+      if(cep.length != 8 || /[a-z]/i.test(cep)) {
+         rspst.innerHTML = ""
+         rspst.style.color = "red"
+         rspst.innerHTML = "O cep deve ter somente 8 NÚMEROS"
+      } else {
+         cep = parseInt(cep)
+         fetch(`https://viacep.com.br/ws/${cep}/json/`)
+         .then(async api => {
+            let resp = await api.json()
+            rspst.style.color = "black"
+            rspst.innerHTML = ""
+            for(let i in resp) {
+               if(i === "erro") {
+                  rspst.innerHTML = "cep não encomtrado."
+                  break
+               }
+               rspst.innerHTML += i + " : " 
+               if(resp[i] === "") {
+                  rspst.innerHTML += "<span class='vl'> ----- <span>"
+               } else {
+                  rspst.innerHTML += `<spn class="vl"> ${resp[i]} </span>`
+               }
+               rspst.innerHTML += "<br>"
             }
-         }
-         if (c == 1) {
-            resp.innerHTML = ""
-            resp.innerHTML = "cep não encontrado "
-         }
-         c = 0
-      })
-   
-      .catch(response => {resp
- 
-         len = 0
-         resp.style.color="red"
-         resp.innerHTML = ""
-         resp.innerHTML += "erro! ×"
-      
-      })
+         })
+         .catch(() => {
+            rspst.style.color = "red"
+            rspst.innerHTML = ""
+            rspst.innerHTML = "Erro, verifique a sua conexão."
+         })
+      }
    }
-}
+})
